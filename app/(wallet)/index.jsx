6bf7293fcef029e9194008441874f890254d33c9
Максим <acoustic.sdk@gmail.com>
2025-09-04 20:34:49 +0300

    Remove all gray backgrounds from wallet

--- app/(wallet)/index.jsx
+++ app/(wallet)/index.jsx
@@ -323,7 +323,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: 'transparent',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
   },
   header: {
     alignItems: 'center',
